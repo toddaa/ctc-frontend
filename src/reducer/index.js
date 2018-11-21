83a73c1ce2e88e85b3ef06e419cb2cfdfe69fe36
Aaron Todd <aarontodd@minitodd.local>
2018-11-21 14:24:36 -0500

    Includes scanlog data

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import scanlog from './scanlog';
 import members from './members';
 import member from './member';
 
 export default combineReducers({
+	scanlog,
 	members,
 	member
 });
