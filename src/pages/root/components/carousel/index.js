ece601498d71afe2f4565addfa6bce80db52fc63
Aaron Todd <aarontodd@minitodd.local>
2018-11-21 16:00:30 -0500

    Utilizes Bootstrap Jumbotron

--- src/pages/root/components/carousel/index.js
+++ src/pages/root/components/carousel/index.js
@@ -3,40 +3,40 @@ import './carousel.css';
 
 const SlideEduSupport = () => (
 	<div className="carousel-item active animated slideInRight">
-		Educational Support
-		<div className="carousel-caption">
-			<h6>Educational Support</h6>
-			<p>From afterschool tutoring to project-based STEM programs (Science, TEchnology, Engineering, Math), Crosswalk Teen Center will reinforce the educational system with support and encouragement for teens.  We guide students into becoming increasingly more future-focused through college and career preparation programs and educational planning.</p>
+		EDUCATIONAL SUPPORT
+		<div className="jumbotron text-white">
+			<h1 className="display-4">Educational Support</h1>
+			<p className="lead">From afterschool tutoring to project-based STEM programs (Science, TEchnology, Engineering, Math), Crosswalk Teen Center will reinforce the educational system with support and encouragement for teens.  We guide students into becoming increasingly more future-focused through college and career preparation programs and educational planning.</p>
 		</div>
 	</div>
 );
 
 const SlideLife = () => (
 	<div className="carousel-item animated slideInRight">
-		Everyday Life
-		<div className="carousel-caption d-none d-md-block">
-			<h6>Everyday Life</h6>
-			<p>Little moments each day shape us into who we are.  Life skills programs provides opportunity for students to grow everyday in little ways - mentally, emotionally, spiritually, and physically.  Programs include making healthy choices, cooking, peer support, and social events to name a few.  We help teens develop in practical living skills in fun and relational ways.</p>
+		EVERYDAY LIFE
+		<div className="jumbotron text-white">
+			<h1 className="display-4">Everyday Life</h1>
+			<p className="lead">Little moments each day shape us into who we are.  Life skills programs provides opportunity for students to grow everyday in little ways - mentally, emotionally, spiritually, and physically.  Programs include making healthy choices, cooking, peer support, and social events to name a few.  We help teens develop in practical living skills in fun and relational ways.</p>
 		</div>
 	</div>
 );
 
 const SlideConnections = () => (
 	<div className="carousel-item animated slideInRight">
-		Community Connections
-		<div className="carousel-caption d-none d-md-block">
-			<h6>Community Connections</h6>
-			<p>Crosswalk Teen CEnter believes students dont just live in a community, but they are part of a community.  Teen committees organize and carry out a wide variety of commuinity projects and partner with local organizations to make our community better.</p>
+		COMMUNITY CONNECTIONS
+		<div className="jumbotron text-white">
+			<h1 className="display-4">Community Connections</h1>
+			<p className="lead">Crosswalk Teen CEnter believes students dont just live in a community, but they are part of a community.  Teen committees organize and carry out a wide variety of commuinity projects and partner with local organizations to make our community better.</p>
 		</div>
 	</div>
 );
 
 const SlideArts= () => (
 	<div className="carousel-item animated slideInRight">
-		Expressive Arts
-		<div className="carousel-caption d-none d-md-block">
-			<h6>Expressive Arts</h6>
-			<p>Crosswalk Teen Center believes students need to espress themselves and use their innate passion and creativity for the good of themselves and the community.  Expressive Art programs are designed for teens to learn and explore various outlets - painting, music, drama, writing, and video production are just a few of the art forms that students may try.</p>
+		EXPRESSIVE ARTS
+		<div className="jumbotron text-white">
+			<h1 className="display-4">Expressive Arts</h1>
+			<p className="lead">Crosswalk Teen Center believes students need to espress themselves and use their innate passion and creativity for the good of themselves and the community.  Expressive Art programs are designed for teens to learn and explore various outlets - painting, music, drama, writing, and video production are just a few of the art forms that students may try.</p>
 		</div>
 	</div>
 );
@@ -69,7 +69,7 @@ class Carousel extends Component {
 				<CarouselIndicators />
 				<div className="carousel-inner">
 					<SlideEduSupport />
-					<SlideLife />
+					<SlideLife /> 
 					<SlideConnections />
 					<SlideArts />
 				</div>
